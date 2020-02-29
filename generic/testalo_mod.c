8961c2ea09731cbd5d8c9494be82497f9d75da35
Red Timmy <40774163+redtimmy@users.noreply.github.com>
2020-02-29 16:07:46 +0100

    Create testalo_mod.c

---
+++ generic/testalo_mod.c
@@ -0,0 +1,11 @@
+// compile with:
+// gcc testalo_mod.c -o testalo_mod.so -shared
+#include <stdio.h>
+#include <stdlib.h>
+
+void testalo()
+{
+   printf("Sucalo Sucalo oh oh!\n");
+   printf("oh oh Sucalo Sucalo!!\n");
+   exit(0);
+}
